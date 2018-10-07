f30ccacdad9b69e8d1fdf77cc096008fb2b021a6
scabbalicious <scabb99@yahoo.com>
2018-10-06 19:03:12 -0700

    begin redoing code and point to assets

---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class Card extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+
+		}
+	}
+	clicked(card){
+		this.props.click(card)
+	}
+
+	render() {
+	return (
+		<div className={"cardContainer" + (!this.props.close ? " opened" : '') + (this.props.complete ? " matched" : '')}
+			onClick={() =>
+				this.clicked(this.props.card)}>
+				<div className='front'>
+					?
+				</div>
+				<div className='back'>
+					<img src={'../assets/' + this.props.card + '.png'}/>
+				</div>
+
+		</div>
+	
+	)
+	}
+}
+
+export default Card
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,51 +1,108 @@
 import React, { Component } from 'react'
 import {shuffler} from "./utilities"
+import Card from './Card'
 
 class Game extends Component {
-	const images = ["&#xf09a","&#xf09a","&#xf427","&#xf427",
+	constructor(props) {
+		super(props)
+		this.state = {
+			cards: ["&#xf09a","&#xf09a","&#xf427","&#xf427",
 '&#xf421','&#xf421','&#xf16d','&#xf16d',
 '&#xf167','&#xf167','&#xf099','&#xf099',
 '&#xf5be','&#xf5be','&#xf1a0','&#xf1a0',
 '&#xf17a','&#xf17a','&#xf179','&#xf179'
-]
-	constructor(props) {
-		super(props)
-		this.state = {
-			cards: shuffler(images.slice()),
-			selected: [],
-			correct: []
+					],
+			duplicatedCards: [],
+			randomCards: [],
+			finalCards: [],
+			selectedCards: []
+		}
+		this.start()
+	}
+	handleClick(name, index){
+		if(this.state.selectedCards.length == 2){
+			setTimeout(() =>{
+				this.check()
+			}, 750)
+		}
+		else {
+			let card = {
+				name,
+				index
+			}
+			let finalCards = this.state.finalCards
+			let cards = this.state.selectedCards
+			finalCards[index].close = false
+			cards.push(card)
+			this.setState({
+				selectedCards: cards,
+				finalCards: finalCards
+			})
+			if(this.state.selectedCards.length == 2){
+				setTimeout(() =>{
+					this.check()
+				}, 750)
+			}
 		}
 	}
 
-	onCardClick(clickedIndex) {
-		const {selected, cards, correct} = this.state
-		if (selected.length === 0) {
-			this.setState({
-				selected: [clickedIndex]
-			})
-		} else if (selected.length === 1) {
-			if(cards[selected[0]] === cards[clickedIndex]) {
-				this.setState({
-					selected: [],
-					correct: correct.concat([selected[0], clickedIndex])
-				})
-			}
-			else {
-				this.setState({
-					selected: [selected[0], clickedIndex]
-				})
-				setTimeout(() => {
-					this.setState({
-						selected: []
-					})
-				}, 1500)
-			}
+	check(){
+		let finalCards = this.state.finalCards
+		if((this.state.selectedCards[0].name == this.state.selectedCards[1].name) && (this.state.selectedCards[0].index != this.state.selectedCards[1].index)){
+
+			finalCards[this.state.selectedCards[0].index].complete = true
+			finalCards[this.state.selectedCards[1].index].complete = true
 		}
+		else {
+			finalCards[this.state.selectedCards[0].index].close = true
+			finalCards[this.state.selectedCards[1].index].close = true
+		}
+			this.setState({
+				finalCards,
+				selectedCards: []
+			})
+	}
+
+	start(){
+		let finalCards = []
+		this.state.duplicatedCards = this.state.cards.concat(this.state.cards)
+		this.state.randomCards = this.shuffle(this.state.duplicatedCards)
+
+		this.state.randomCards.map((name,index) => {
+			finalCards.push({
+				name,
+				close: true,
+				complete: false,
+				fail: false
+			})
+		})
+		this.state.finalCards = finalCards
+	}
+	shuffle(array){
+		let currentIndex = array.length, temporaryValue, randomIndex
+		while (0 != currentIndex) {
+			randomIndex = Math.floor(Math.random()*currentIndex)
+			currentIndex -= 1
+			temporaryValue = array[currentIndex]
+			array[currentIndex] = array[randomIndex]
+			array[randomIndex] = temporaryValue
+		}
+		return array
 	}
 
 	render() {
 	return (
-	
+		<div className="gameContainer">
+			{this.state.finalCards.map((card, index) => {
+				return
+					<Card card={card.name}
+					click={() => {this.handleClick(card.name, index)}}
+					close={card.close}
+					complete={card.complete}
+					/>
+			})
+			}
+		</div>
 	)
 	}
 }
